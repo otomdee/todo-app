123a61e5564c6009c01d965fa1e4649d9c1a322e
otomdee <otomdaniel65@gmail.com>
2024-09-02 15:11:44 +0100

    add task and project classes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
 import "./styles.css";
+
+let currentProjects = [];
+let currentTasks = [];
+
+class Task {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+        changeTitle(newTitle) {
+            this.title = newTitle;
+        }
+        changeDescription(newDescription) {
+            this.description = newDescription;
+        }
+        changeDate(newDate) {
+            this.dueDate = newDate;
+        }
+        changePriority(newPriority) {
+            this.priority = newPriority;
+        }
+}
+
+class Project {
+    constructor(tasksArray) {
+        this.tasksArray = tasksArray;
+    }
+
+    addTask(task) {
+        this.tasksArray.push(task);
+    }
+}
+
+function addTask(task) {
+    currentProjects.push(task);
+}
+
+function addTaskToProject(task, project) {
+    project.tasksArray.push(task);
+}
+
+function addProject(project) {
+
+}
--- src/tasks.js
+++
@@ -1,8 +0,0 @@
-export class Task {
-    constructor(title, description, dueDate, priority) {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-    }
-}
