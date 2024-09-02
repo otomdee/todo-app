0b4fc6794c97a612b7379dcb36213c905c8219f6
otomdee <otomdaniel65@gmail.com>
2024-09-02 17:09:09 +0100

    seperate tasks js file

--- src/index.js
+++ src/index.js
@@ -1,47 +1,6 @@
 import "./styles.css";
+import {Task, Project, currentProjects, currentTasks} from "./tasks.js";
 
-let currentProjects = [];
-let currentTasks = [];
-
-class Task {
-    constructor(title, description, dueDate, priority) {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-    }
-        changeTitle(newTitle) {
-            this.title = newTitle;
-        }
-        changeDescription(newDescription) {
-            this.description = newDescription;
-        }
-        changeDate(newDate) {
-            this.dueDate = newDate;
-        }
-        changePriority(newPriority) {
-            this.priority = newPriority;
-        }
-}
-
-class Project {
-    constructor(tasksArray) {
-        this.tasksArray = tasksArray;
-    }
-
-    addTask(task) {
-        this.tasksArray.push(task);
-    }
-}
-
-function addTask(task) {
-    currentProjects.push(task);
-}
-
-function addTaskToProject(task, project) {
-    project.tasksArray.push(task);
-}
-
-function addProject(project) {
-
-}
+document.querySelector("#newTask").addEventListener("click", () => {
+    document.querySelector("#taskDialog").showModal();
+})
---
+++ src/tasks.js
@@ -0,0 +1,44 @@
+let currentProjects = [];
+let currentTasks = [];
+
+class Task {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+        changeTitle(newTitle) {
+            this.title = newTitle;
+        }
+        changeDescription(newDescription) {
+            this.description = newDescription;
+        }
+        changeDate(newDate) {
+            this.dueDate = newDate;
+        }
+        changePriority(newPriority) {
+            this.priority = newPriority;
+        }
+        addTask() {
+            currentTasks.push(this);
+        }
+}
+
+class Project {
+    constructor(title, tasksArray) {
+        this.title = title;
+        this.tasksArray = tasksArray;
+    }
+
+    addTask(task) {
+        this.tasksArray.push(task);
+    }
+}
+
+function addProject(projectName) {
+    const project = new Project(projectName, []);
+    currentProjects.push(project);
+}
+
+export {Task, Project, currentProjects, currentTasks};
