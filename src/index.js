26619b9a07eb6bf8fc06c1a7c6fd5dbc8ef8a205
otomdee <otomdaniel65@gmail.com>
2024-09-04 23:11:32 +0100

    add task render

--- src/index.js
+++ src/index.js
@@ -20,29 +20,61 @@ document.querySelector("#taskDialog").addEventListener("click", e => {
 })
 
 
-//fxn addTaskToDOM
+//fxn render task
 function renderTask(task) {
     const card = document.createElement("div");
+    const checkbox = document.createElement("input");
     const cardTitle = document.createElement("span");
     const cardDesc = document.createElement("span");
     const cardDuedate = document.createElement("span");
     const cardPriority = document.createElement("span");
 
 
-    [cardTitle, cardDesc, cardDuedate, cardPriority].forEach((item) => {
+    [checkbox, cardTitle, cardDesc, cardDuedate, cardPriority].forEach((item) => {
         item.classList.add("inline");
         card.append(item);
     })
     card.classList.add("taskCard");
-
+    checkbox.type = "checkbox";
 
     cardTitle.innerHTML = task.title;
     cardDesc.innerHTML = task.description;
-    cardDuedate.innerHTML = task.duedate;
+    cardDuedate.innerHTML = task.dueDate;
     cardPriority.innerHTML = task.priority;
+
+    if (task.priority === "high") {
+        cardPriority.classList.add("highPriority");
+    }
+    else if (task.priority === "mid") {
+        cardPriority.classList.add("midPriority")
+    }
+    else if (task.priority === "low") {
+        cardPriority.classList.add("lowPriority");
+    }
+
 
     document.querySelector("#mainPage").append(card);
 }
 
-const mytask = new Task("bread", "buy bread", "2024-09-03", "high");
+//dummy tasks
+const mytask = new Task("bread", "buy bread", "2024-09-03", "low");
 renderTask(mytask);
+const task2 = new Task("paint", "get brush", "2024-09-03", "mid");
+renderTask(task2);
+
+//take modal input, create task with it, render project
+const form = document.querySelector("#taskForm");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    const title = document.querySelector("#titleInput");
+    const description = document.querySelector("#descriptionInput");
+    const date = document.querySelector("#dateInput");
+    const priority = document.querySelector("input[name=priority]:checked");
+
+    const task = new Task (title.value, description.value, date.value, priority.value);
+
+    renderTask(task);
+    form.reset();
+    document.querySelector("#taskDialog").close();
+})
--- src/tasks.js
+++ src/tasks.js
@@ -22,7 +22,6 @@ class Task {
         }
         addTask() {
             currentTasks.push(this);
-
         }
 }
 
